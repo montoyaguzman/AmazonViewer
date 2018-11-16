3e9909a159cee3c6f771e7c05a15039c7133e2f1
montoyaguzman <montoyaguzman7@gmail.com>
2018-11-15 18:43:24 -0600

    proyecto sin herencia

---
+++ src/com/montoyita/amazonviewer/model/Book.java
@@ -0,0 +1,90 @@
+package com.montoyita.amazonviewer.model;
+
+import java.util.Date;
+
+public class Book {
+	
+	private int id;
+	private String title;
+	private Date date;
+	private String editorial;
+	private String authors[];
+	private String isbn;
+	private boolean readed;
+	private int timeReaded;
+	
+	public Book(String title, Date date, String editorial, String isbn) {
+		super();
+		this.title = title;
+		this.date = date;
+		this.editorial = editorial;
+		this.isbn = isbn;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public Date getDate() {
+		return date;
+	}
+
+	public void setDate(Date date) {
+		this.date = date;
+	}
+
+	public String getEditorial() {
+		return editorial;
+	}
+
+	public void setEditorial(String editorial) {
+		this.editorial = editorial;
+	}
+
+	public String[] getAuthors() {
+		return authors;
+	}
+
+	public void setAuthors(String[] authors) {
+		this.authors = authors;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public boolean isReaded() {
+		return readed;
+	}
+
+	public void setReaded(boolean readed) {
+		this.readed = readed;
+	}
+
+	public int getTimeReaded() {
+		return timeReaded;
+	}
+
+	public void setTimeReaded(int timeReaded) {
+		this.timeReaded = timeReaded;
+	}
+	
+	
+	
+}
---
+++ src/com/montoyita/amazonviewer/model/Serie.java
@@ -0,0 +1,87 @@
+package com.montoyita.amazonviewer.model;
+
+public class Serie {
+	
+	private int id;
+	private String title;
+	private String genre;
+	private String creator;
+	private int duration;
+	private short year;
+	private boolean viewed;
+	private int timeViewed;
+	// chapters[]
+	
+	public Serie(String title, String genre, int duration) {
+		super();
+		this.title = title;
+		this.genre = genre;
+		this.duration = duration;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getGenre() {
+		return genre;
+	}
+
+	public void setGenre(String genre) {
+		this.genre = genre;
+	}
+
+	public String getCreator() {
+		return creator;
+	}
+
+	public void setCreator(String creator) {
+		this.creator = creator;
+	}
+
+	public int getDuration() {
+		return duration;
+	}
+
+	public void setDuration(int duration) {
+		this.duration = duration;
+	}
+
+	public short getYear() {
+		return year;
+	}
+
+	public void setYear(short year) {
+		this.year = year;
+	}
+
+	public boolean isViewed() {
+		return viewed;
+	}
+
+	public void setViewed(boolean viewed) {
+		this.viewed = viewed;
+	}
+
+	public int getTimeViewed() {
+		return timeViewed;
+	}
+
+	public void setTimeViewed(int timeViewed) {
+		this.timeViewed = timeViewed;
+	}
+	
+	
+}
